2a2f018a80affe7ff36363b8c438b6c18c3675e9
Xavier Tilley <hippietilley@gmail.com>
2018-05-17 16:13:58 -0500

    End of day commit

--- motionsense/src/main/java/org/md2k/motionsense/PrefsFragmentSettings.java
+++ motionsense/src/main/java/org/md2k/motionsense/PrefsFragmentSettings.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.motionsense;
 
 import android.bluetooth.BluetoothDevice;
@@ -36,87 +63,82 @@ import rx.Observer;
 import rx.Subscription;
 
 /**
- * Copyright (c) 2015, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
- * All rights reserved.
- * <p/>
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * <p/>
- * * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * <p/>
- * * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * <p/>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Preferences Fragment for this application's settings.
  */
 public class PrefsFragmentSettings extends PreferenceFragment {
     Subscription scanSubscription;
     HashMap<String, BluetoothDevice> devices;
 
-
+    /**
+     * Creates a new <code>devices</code> hashmap.
+     * @param savedInstanceState This activity's previous state, is null if this activity has never
+     *                           existed.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        devices=new HashMap<>();
+        devices = new HashMap<>();
         addPreferencesFromResource(R.xml.pref_settings);
         setPreferenceScreenConfigured();
     }
 
+    /**
+     * Calls <code>scan()</code> when resumed.
+     */
     @Override
     public void onResume() {
         scan();
         super.onResume();
     }
 
+    /**
+     *
+     */
     void scan() {
         RxBleClient rxBleClient = MyApplication.getRxBleClient(getActivity());
-        scanSubscription = rxBleClient.scanBleDevices(
-                new ScanSettings.Builder()
-                        // .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY) // change if needed
-                        // .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES) // change if needed
-                        .build()
-                // add filters if needed
-        ).subscribe(new Observer<ScanResult>() {
+        scanSubscription = rxBleClient.scanBleDevices(new ScanSettings.Builder().build())
+                .subscribe(new Observer<ScanResult>() {
             @Override
-            public void onCompleted() {
+            public void onCompleted() {}
 
-            }
-
+                    /**
+                     * Displays a toast notification when an error occurs.
+                     * @param e Exception that was thrown.
+                     */
             @Override
             public void onError(Throwable e) {
                 Toast.makeText(getActivity(), "!!! ERROR !!! e=" + e.toString(), Toast.LENGTH_LONG).show();
                 getActivity().finish();
             }
 
+                    /**
+                     *
+                     * @param scanResult Result of the scan.
+                     */
             @Override
             public void onNext(ScanResult scanResult) {
                 String name = scanResult.getScanRecord().getDeviceName();
                 List<ParcelUuid> p = scanResult.getScanRecord().getServiceUuids();
-                if (p == null || p.size() != 1 || name == null) return;
-                if (!(MotionSense.is(name, p.get(0).toString()) || MotionSenseHRV.is(name, p.get(0).toString()) || MotionSenseHRVPlus.is(name, p.get(0).toString())))
+                if (p == null || p.size() != 1 || name == null)
+                    return;
+                if (!(MotionSense.is(name, p.get(0).toString()) ||
+                        MotionSenseHRV.is(name, p.get(0).toString()) ||
+                        MotionSenseHRVPlus.is(name, p.get(0).toString())))
                     return;
                 if(!devices.containsKey(scanResult.getBleDevice().getMacAddress()))
-                    devices.put(scanResult.getBleDevice().getMacAddress(), scanResult.getBleDevice().getBluetoothDevice());
+                    devices.put(scanResult.getBleDevice().getMacAddress(),
+                            scanResult.getBleDevice().getBluetoothDevice());
                 if (ConfigurationManager.isConfigured(scanResult.getBleDevice().getMacAddress()))
                     return;
                 if (MotionSense.is(name, p.get(0).toString()))
-                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE, scanResult.getBleDevice().getMacAddress());
+                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE,
+                            scanResult.getBleDevice().getMacAddress());
                 else if (MotionSenseHRV.is(name, p.get(0).toString()))
-                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE_HRV, scanResult.getBleDevice().getMacAddress());
+                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE_HRV,
+                            scanResult.getBleDevice().getMacAddress());
                 else
-                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE_HRV_PLUS, scanResult.getBleDevice().getMacAddress());
+                    addToPreferenceScreenAvailable(PlatformType.MOTION_SENSE_HRV_PLUS,
+                            scanResult.getBleDevice().getMacAddress());
             }
         });
     }
