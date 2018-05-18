01c552baea6574676dcbb8649744037365ac757d
Xavier Tilley <hippietilley@gmail.com>
2018-05-18 15:55:49 -0500

    Documented MetaData

--- motionsense/src/main/java/org/md2k/motionsense/configuration/MetaData.java
+++ motionsense/src/main/java/org/md2k/motionsense/configuration/MetaData.java
@@ -1,7 +1,6 @@
-package org.md2k.motionsense.configuration;
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,6 +25,8 @@ package org.md2k.motionsense.configuration;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package org.md2k.motionsense.configuration;
+
 import android.content.Context;
 
 import org.md2k.datakitapi.source.datasource.DataSource;
@@ -34,8 +35,17 @@ import org.md2k.mcerebrum.commons.storage.Storage;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * Provides methods for accessing metadata.
+ */
 class MetaData {
     private static final String FILENAME_ASSET_METADATA = "metadata.json";
+
+    /**
+     * Returns an arraylist of <code>DataSource</code>s in the metadata file.
+     * @param context Android context
+     * @return An arraylist of <code>DataSource</code>s in the metadata file.
+     */
     private static ArrayList<DataSource> readMetaData(Context context) {
         try {
             return Storage.readJsonArrayFromAsset(context, FILENAME_ASSET_METADATA, DataSource.class);
@@ -44,21 +54,39 @@ class MetaData {
         }
     }
 
+    /**
+     * Returns a <code>DataSource</code> that matches the given parameters.
+     * @param context Android context
+     * @param dataSourceType Data type of the <code>DataSource</code>.
+     * @param dataSourceId Id of the <code>DataSource</code>.
+     * @param platformType Platform type of the <code>DataSource</code>.
+     * @return A <code>DataSource</code> that matches the given parameters.
+     */
     static DataSource getDataSource(Context context, String dataSourceType, String dataSourceId, String platformType) {
-        ArrayList<DataSource> metaData=readMetaData(context);
-        for(int i=0;metaData!=null && i<metaData.size();i++){
-            if(!metaData.get(i).getType().equals(dataSourceType)) continue;
-            if(!metaData.get(i).getPlatform().getType().equals(platformType)) continue;
-            if(dataSourceId==null && metaData.get(i).getId()==null) return metaData.get(i);
-            if(dataSourceId!=null && metaData.get(i).getId()!=null && dataSourceId.equals(metaData.get(i).getId())) return metaData.get(i);
+        ArrayList<DataSource> metaData = readMetaData(context);
+        for(int i = 0; metaData != null && i < metaData.size(); i++){
+            if(!metaData.get(i).getType().equals(dataSourceType))
+                continue;
+            if(!metaData.get(i).getPlatform().getType().equals(platformType))
+                continue;
+            if(dataSourceId == null && metaData.get(i).getId() == null)
+                return metaData.get(i);
+            if(dataSourceId != null && metaData.get(i).getId()!= null && dataSourceId.equals(metaData.get(i).getId()))
+                return metaData.get(i);
         }
         return null;
     }
 
+    /**
+     * Returns an arraylist of <code>DataSource</code>s that match the given type.
+     * @param context Android context
+     * @param type Data type to look for.
+     * @return An arraylist of <code>DataSource</code>s that match the given type.
+     */
     static ArrayList<DataSource> getDataSources(Context context, String type) {
-        ArrayList<DataSource> metaData=readMetaData(context);
-        ArrayList<DataSource> dataSources=new ArrayList<>();
-        for(int i=0;metaData!=null && i<metaData.size();i++){
+        ArrayList<DataSource> metaData = readMetaData(context);
+        ArrayList<DataSource> dataSources = new ArrayList<>();
+        for(int i = 0; metaData != null && i < metaData.size(); i++){
             if(metaData.get(i).getPlatform().getType().equals(type)){
                 dataSources.add(metaData.get(i));
             }
