de41a37b1b49556e389cdd0fb322e3a388cfe1aa
Xavier Tilley <hippietilley@gmail.com>
2018-05-18 13:36:34 -0500

    Documented PrefsFragmentPlot

--- motionsense/src/main/java/org/md2k/motionsense/plot/PrefsFragmentPlot.java
+++ motionsense/src/main/java/org/md2k/motionsense/plot/PrefsFragmentPlot.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.motionsense.plot;
 
 import android.content.Intent;
@@ -21,43 +48,17 @@ import org.md2k.motionsense.configuration.ConfigurationManager;
 import java.util.ArrayList;
 
 /**
- * Copyright (c) 2015, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
- * All rights reserved.
- * <p/>
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * <p/>
- * * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * <p/>
- * * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * <p/>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Preferences Fragment for Plot
  */
 public class PrefsFragmentPlot extends PreferenceFragment {
-/*
-    Preference.OnPreferenceChangeListener onPreferenceChangeListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            phoneSensorDataSources.find(preference.getKey()).setEnabled((Boolean) newValue);
-            updatePreferenceScreen();
-            return false;
-        }
-    };
-*/
 
+    /**
+     * Reads configuration, inflates <code>R.xml.pref_plot_choice</code> and calls
+     * <code>createPreferencesScreen</code>.
+     *
+     * @param savedInstanceState This activity's previous state, is null if this activity has never
+     *                           existed.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +66,15 @@ public class PrefsFragmentPlot extends PreferenceFragment {
         addPreferenceScreenSensors();
     }
 
+    /**
+     * Creates the settings view
+     *
+     * @param inflater Android LayoutInflater
+     * @param container Android ViewGroup
+     * @param savedInstanceState This activity's previous state, is null if this activity has never
+     *                           existed.
+     * @return The view this method created.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -75,19 +85,26 @@ public class PrefsFragmentPlot extends PreferenceFragment {
         return v;
     }
 
-
+    /**
+     * Creates a preference object for the given data source type.
+     * @param dataSourceType Data source type
+     * @param dataSourceId Data source id
+     * @param platformType Platform type
+     * @param platformId Platform id
+     * @return The created <code>Preference</code> object.
+     */
     private Preference createPreference(String dataSourceType, String dataSourceId, String platformType, String platformId) {
-
         Preference preference = new Preference(getActivity());
         preference.setKey(dataSourceType);
         String title = dataSourceType;
         title = title.replace("_", " ");
         title = title.substring(0, 1).toUpperCase() + title.substring(1).toLowerCase();
-        if(dataSourceId!=null) title+=" ("+dataSourceId+")";
+        if(dataSourceId != null)
+            title += " ("+dataSourceId+")";
         preference.setTitle(title);
         preference.setSummary(platformId);
         preference.setOnPreferenceClickListener(preference1 -> {
-            Intent intent=new Intent(getActivity(), ActivityPlot.class);
+            Intent intent = new Intent(getActivity(), ActivityPlot.class);
             Platform p = new PlatformBuilder().setId(platformId).setType(platformType).build();
             DataSource d = new DataSourceBuilder().setType(dataSourceType).setId(dataSourceId).setPlatform(p).build();
             Bundle bundle = new Bundle();
@@ -99,20 +116,31 @@ public class PrefsFragmentPlot extends PreferenceFragment {
         return preference;
     }
 
+    /**
+     * Adds sensors to the preference screen.
+     *
+     * <p>
+     * Removes all data source type preferences before iterating through the
+     * <code>phoneSensorDataSources</code> ArrayList, creating a preference for each data source and
+     * adding it to the category.
+     * </p>
+     */
     protected void addPreferenceScreenSensors() {
         String dataSourceType, platformId, dataSourceId, platformType;
         PreferenceCategory preferenceCategory = (PreferenceCategory) findPreference("dataSourceType");
         preferenceCategory.removeAll();
         ArrayList<DataSource> dataSources = ConfigurationManager.read(getActivity());
         for (int i = 0; i < dataSources.size(); i++) {
-                platformId=dataSources.get(i).getPlatform().getId();
-            platformType=dataSources.get(i).getPlatform().getType();
-                dataSourceType=dataSources.get(i).getType();
-                dataSourceId = dataSources.get(i).getId();
-                if(dataSourceType.equals(DataSourceType.RAW)) continue;
-                if(dataSourceType.equals(DataSourceType.DATA_QUALITY)) continue;
-                Preference preference = createPreference(dataSourceType, dataSourceId, platformType, platformId);
-                preferenceCategory.addPreference(preference);
+            platformId = dataSources.get(i).getPlatform().getId();
+            platformType = dataSources.get(i).getPlatform().getType();
+            dataSourceType = dataSources.get(i).getType();
+            dataSourceId = dataSources.get(i).getId();
+            if(dataSourceType.equals(DataSourceType.RAW))
+                continue;
+            if(dataSourceType.equals(DataSourceType.DATA_QUALITY))
+                continue;
+            Preference preference = createPreference(dataSourceType, dataSourceId, platformType, platformId);
+            preferenceCategory.addPreference(preference);
         }
     }
 }
