9626e1b30e13d0ae3b7bd370ffaabb61c67233a6
Xavier Tilley <hippietilley@gmail.com>
2018-05-22 12:24:04 -0500

    Documented ErrorNotify

--- motionsense/src/main/java/org/md2k/motionsense/error/ErrorNotify.java
+++ motionsense/src/main/java/org/md2k/motionsense/error/ErrorNotify.java
@@ -1,7 +1,6 @@
-package org.md2k.motionsense.error;
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,6 +25,8 @@ package org.md2k.motionsense.error;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package org.md2k.motionsense.error;
+
 import android.content.Context;
 import android.os.Bundle;
 
@@ -34,14 +35,35 @@ import org.md2k.motionsense.R;
 
 import br.com.goncalves.pugnotification.notification.PugNotification;
 
+/**
+ * Provides methods for creating, displaying, and removing error notifications.
+ * <p>
+ *     Possible types of error notifications are:
+ *     <ul>
+ *         <li><code>PERMISSION</code> with an integer value of 0.</li>
+ *         <li><code>BLUETOOTH_OFF</code> with an integer value of 1.</li>
+ *         <li><code>GPS_OFF</code> with an integer value of 2.</li>
+ *         <li><code>NOT_CONFIGURED</code> with an integer value of 3.</li>
+ *         <li><code>DATAKIT_CONNECTION_ERROR</code> with an integer value of 4.</li>
+ *         <li><code>DATAKIT_CONNECTION_ERROR</code> with an integer value of 5.</li>
+ *         <li><code>DATAKIT_INSERT_ERROR</code> with an integer value of 6.</li>
+ *     </ul>
+ * </p>
+ */
 public class ErrorNotify {
-    public static final int PERMISSION=0;
-    public static final int BLUETOOTH_OFF=1;
-    public static final int GPS_OFF=2;
-    public static final int NOT_CONFIGURED=3;
-    public static final int DATAKIT_CONNECTION_ERROR =4;
-    public static final int DATAKIT_REGISTRATION_ERROR=5;
-    public static final int DATAKIT_INSERT_ERROR=6;
+    public static final int PERMISSION = 0;
+    public static final int BLUETOOTH_OFF = 1;
+    public static final int GPS_OFF = 2;
+    public static final int NOT_CONFIGURED = 3;
+    public static final int DATAKIT_CONNECTION_ERROR = 4;
+    public static final int DATAKIT_REGISTRATION_ERROR = 5;
+    public static final int DATAKIT_INSERT_ERROR = 6;
+
+    /**
+     * Creates the appropriate notification based on the given type of error.
+     * @param context Android context
+     * @param type Type of error.
+     */
     public static void handle(Context context, int type){
      switch(type){
          case PERMISSION:
@@ -54,26 +76,33 @@ public class ErrorNotify {
              showNotification(context, "MotionSense App: GPS off", "(Please click to turn on GPS)");
              break;
          case NOT_CONFIGURED:
-//             showNotification("MotionSense App: GPS off", "(Please click to turn on GPS)");
              break;
          case DATAKIT_CONNECTION_ERROR:
-//             showNotification("MotionSense App: GPS off", "(Please click to turn on GPS)");
              break;
          case DATAKIT_REGISTRATION_ERROR:
-//             showNotification("MotionSense App: GPS off", "(Please click to turn on GPS)");
              break;
          case DATAKIT_INSERT_ERROR:
-//             showNotification("MotionSense App: GPS off", "(Please click to turn on GPS)");
              break;
-
      }
     }
+
+    /**
+     * Shows a notification with the given title and message.
+     * @param context Android context
+     * @param title Title of the notification.
+     * @param message Message of the notification.
+     */
     private static void showNotification(Context context, String title, String message) {
         Bundle bundle = new Bundle();
         bundle.putInt(ActivityMain.OPERATION, ActivityMain.OPERATION_START_BACKGROUND);
         PugNotification.with(context).load().identifier(21).title(title).smallIcon(R.mipmap.ic_launcher)
                 .message(message).autoCancel(true).click(ActivityMain.class, bundle).simple().build();
     }
+
+    /**
+     * Removes a notification.
+     * @param context Android context
+     */
     public static void removeNotification(Context context) {
         PugNotification.with(context).cancel(21);
     }
